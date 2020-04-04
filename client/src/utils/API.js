ad1a9eeba59c36c149e760c3f79d0ae302afe8db
stuart lamble <lamble.stuart@googlemail.com>
2020-04-04 16:18:22 +0100

    added front end API routes & methos

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,9 +1,30 @@
 import axios from "axios";
 
-// The getRecipes method retrieves recipes from the server
-// It accepts a "query" or term to search the recipe api for
 export default {
-  getRecipes: function(query) {
-    return axios.get("/api/recipes", { params: { q: query } });
+  // Gets books from the Google API
+  getBooks: function(req, res){
+    return axios.get("https://www.googleapis.com/books/v1/volumes", {params})
+    .then(results =>
+      results.data.items.filter(
+        result =>
+          result.volumeInfo.title &&
+          result.volumeInfo.infoLink &&
+          result.volumeInfo.authors &&
+          result.volumeInfo.description &&
+          result.volumeInfo.imageLinks &&
+          result.volumeInfo.imageLinks.thumbnail
+      )
+    )
+    .then(books => res.json(books))
+    .catch(err => res.status(422).json(err))
+  },
+
+  // Gets all saved books
+  getSavedBooks: function() {
+    return axios.get("/api/books");
+  },
+  // Saves an book to the database
+  saveBook: function(bookData) {
+    return axios.post("/api/books", bookData);
   }
 };
