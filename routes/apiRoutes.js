0db8d90e398098c0ecf205d4c6541a23bcaf2e9d
stuart lamble <lamble.stuart@googlemail.com>
2020-04-05 13:29:04 +0100

    Updated index.js, App.js, added page folders

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,18 @@
 const router = require("express").Router();
 const db = require("../models");
 
-router.get("/recipes", (req, res) => {
-  // Use a regular expression to search titles for req.query.q
-  // using case insensitive match. https://docs.mongodb.com/manual/reference/operator/query/regex/index.html
-  db.Recipe.find({
-    title: { $regex: new RegExp(req.query.q, 'i')}
-  })
-    .then(recipes => res.json(recipes))
+router.get("/books", (req, res) => {
+  //find all books (saved to database by user)
+  db.Books.find()
+    .then(dbBooks => res.json(dbBooks))
     .catch(err => res.status(422).end());
 });
+
+router.post("/books", (req, res)=>
+  //add a book to database
+  db.Books.create(req.body)
+    .then(dbBooks => res.json(dbBooks))
+    .catch(err => res.status(422).json(err))
+)
 
 module.exports = router;
